9403413492b945a006e95eac5df5133f9981c086
yssin777 <yassinnm09@gmail.com>
2026-02-28 12:02:07 +0100

    creacion inicial de registro e inicio de sesion

--- Fixia.sql
+++ Fixia.sql
@@ -15,7 +15,7 @@ CREATE TABLE talleres (
 CREATE TABLE usuarios (
     id INT AUTO_INCREMENT PRIMARY KEY,
     taller_id INT NOT NULL,
-    rol ENUM('supervisor', 'recepcionista', 'mecanico', 'cliente') NOT NULL,
+    rol ENUM('ceo','jefe', 'recepcionista', 'mecanico', 'cliente') NOT NULL,
     nombre_completo VARCHAR(100) NOT NULL,
     email VARCHAR(100) NOT NULL,
     password_hash VARCHAR(255) NOT NULL,
